6a6a43986aaf7f3e0fa280c2ff4b5d6be1a16f4c
bratwursted <joe@thinxmedia.com>
2013-12-03 18:46:19 -0500

    Initial Commit

---
+++ TumblrSlideshow/TSSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSSAppDelegate.h
+//  TumblrSlideshow
+//
+//  Created by Joe on 12/3/13.
+//  Copyright (c) 2013 Joe Bramhall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TumblrSlideshow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TumblrSlideshow
+//
+//  Created by Joe on 12/3/13.
+//  Copyright (c) 2013 Joe Bramhall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSSAppDelegate class]));
+    }
+}
